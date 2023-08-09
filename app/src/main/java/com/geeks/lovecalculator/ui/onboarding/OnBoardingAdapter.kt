7dd188e678be5c2f94a512012050fcd7ed643a39
Kiberpraim <abdykulovalmanbet4@gmail.com>
2023-08-09 12:34:47 +0600

    Home work 8

--- app/src/main/java/com/geeks/lovecalculator/ui/onboarding/OnBoardingAdapter.kt
+++ app/src/main/java/com/geeks/lovecalculator/ui/onboarding/OnBoardingAdapter.kt
@@ -6,6 +6,7 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import coil.load
+import com.geeks.lovecalculator.R
 import com.geeks.lovecalculator.databinding.ItemOnBoardingBinding
 import com.geeks.lovecalculator.model.OnBoarding
 
@@ -17,23 +18,23 @@ class OnBoardingAdapter(private val onClick: () -> Unit) :
         OnBoarding(
             "Have a good time",
             "You should take the time \nto help those who need you",
-            "https://i.postimg.cc/cHhP682g/Group-3.png"
+            R.raw.animation_hello
         ),
         OnBoarding(
             "Cherishing love",
             "It is now no longer possible for\n" +
                     "you to cherish love",
-            "https://i.postimg.cc/054RR7sr/image-5.png"
+            R.raw.animation_love2
         ),
         OnBoarding(
             "Have a breakup?",
             "We have made the correction for you\n dont worry\nMaybe someone is waiting for you!",
-            "https://i.postimg.cc/hP7GHSHW/image-6.png"
+            R.raw.animation_love
         ),
         OnBoarding(
             "It's Funs and Many more",
             "",
-            "https://i.postimg.cc/3xr7BYCH/image-7.png"
+            R.raw.animation_couple
         ),
     )
 
@@ -59,7 +60,7 @@ class OnBoardingAdapter(private val onClick: () -> Unit) :
             with(binding) {
                 tvTitle.text = onBoarding.title
                 tvDescription.text = onBoarding.description
-                imageOnBoarding.load(onBoarding.image)
+                lottieAnimationView.setAnimation(onBoarding.anim!!)
                 btnStart.isVisible = adapterPosition == list.lastIndex
                 btnStart.setOnClickListener {
                     onClick()
